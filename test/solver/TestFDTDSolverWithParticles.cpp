ab1c76172c6b5206145517498eb1adcf9e71b485
Manuel <manuel.winkler@gmx.ch>
2024-05-04 16:03:34 +0200

    Fix rendering linkage and example

--- test/solver/TestFDTDSolverWithParticles.cpp
+++ test/solver/TestFDTDSolverWithParticles.cpp
@@ -5,8 +5,7 @@ using std::size_t;
 #include "Types/Vector.h"
 #include "Field/Field.h"
 #include "MaxwellSolvers/FDTD.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
+#include "Utility/Rendering.hpp"
 
 #include <Kokkos_Random.hpp>
 template<typename scalar1, typename... scalar>
@@ -34,12 +33,14 @@ int main(int argc, char* argv[]){
         //using vector4_type = ippl::Vector<scalar, 4>;
         //using FourField = ippl::Field<vector4_type, dim, ippl::UniformCartesian<scalar, dim>, typename ippl::UniformCartesian<scalar, dim>::DefaultCentering>;
         //using ThreeField = ippl::Field<vector_type, dim, ippl::UniformCartesian<scalar, dim>, typename ippl::UniformCartesian<scalar, dim>::DefaultCentering>;
-        constexpr size_t n = 100;
+        constexpr size_t n = 50;
         ippl::Vector<uint32_t, 3> nr{n / 2, n / 2, 2 * n};
         ippl::NDIndex<3> owned(nr[0], nr[1], nr[2]);
-        ippl::Vector<scalar, 3> extents{meter_in_unit_lengths,
-                                        meter_in_unit_lengths,
-                                        meter_in_unit_lengths};
+                               
+        ippl::Vector<scalar, 3> extents{1.0,
+                                        1.0,
+                                        1.0};
+
         std::array<bool, 3> isParallel;
         isParallel.fill(false);
         isParallel[2] = true;
@@ -54,7 +55,8 @@ int main(int argc, char* argv[]){
         ippl::Vector<scalar, 3> origin = {0,0,0};
         ippl::UniformCartesian<scalar, 3> mesh(owned, hx, origin);
         
-        ippl::NSFDSolverWithParticles<scalar, ippl::absorbing> solver(layout, mesh, 1 << 15);
+        ippl::NSFDSolverWithParticles<scalar, ippl::absorbing> solver(layout, mesh, 1 << 5);
+
         
         auto pview = solver.particles.R.getView();
         auto p1view = solver.particles.R_nm1.getView();
@@ -79,10 +81,22 @@ int main(int argc, char* argv[]){
             std::cout << ippl::Vector<double, 3>(var * (1.0 / solver.particles.getLocalNum())) << "\n";
         }
         solver.playout.update(solver.particles);
-        solver.particles.Q = electron_charge_in_unit_charges;
+        solver.particles.Q = electron_charge_in_unit_charges * 1000;
         solver.particles.mass = electron_mass_in_unit_masses;
-        for(int i = 0;i < 200;i++){
+        for(int i = 0;i < 1200;i++){
             solver.solve();
+            if(true){
+                using vec3 = rm::Vector<float, 3>;
+                //ippl::Image img = ippl::drawFieldCrossSection(solver.E, 1000, 1000, ippl::axis::x, extents[0] / 2, KOKKOS_LAMBDA(const ippl::Vector<scalar, 3>& E){
+                //    return ippl::normalized_colormap(turbo_cm, Kokkos::sqrt(E.dot(E)) / 5000.0f);
+                //});
+                rm::camera cam(vec3{0.5,0.5,-1}, vec3{0,0,1});
+                ippl::Image img = ippl::drawParticles(solver.particles.R, 700, 700, cam, 0.01f, ippl::Vector<float, 4>{0,1,0,1});
+                ippl::Image img2 = ippl::drawFieldFog(solver.E, 700, 700,cam, KOKKOS_LAMBDA(const ippl::Vector<scalar, 3>& E){
+                    return ippl::normalized_colormap(turbo_cm, Kokkos::sqrt(E.dot(E)) / 5000.0f);
+                }, img);
+                img2.save_to("renderdata/" + std::format("{:05d}.png", i));
+            }
         }
         {
             double var = 0;
